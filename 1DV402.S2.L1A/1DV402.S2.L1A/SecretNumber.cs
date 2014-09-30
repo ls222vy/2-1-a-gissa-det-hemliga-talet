d75206f8289ca5c564fe1efd817cb447672ec881
unknown <ls222vy@NY108X10.lnu.se>
2014-09-30 16:16:11 +0200

    starting lab 2.1

---
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L1A
+{
+   public class SecretNumber
+    {
+        private int _count;
+        private int _number;
+       
+        public const int MaxNumberOfGuesses = 7;
+
+        public void Initialize() 
+        {
+            Random myRandom = new Random();
+            _number = myRandom.Next(1, 101);
+            _count = 0;
+        }
+        public bool MakeGuess(int number)
+        {
+            return true;
+        }    
+    }
+}
