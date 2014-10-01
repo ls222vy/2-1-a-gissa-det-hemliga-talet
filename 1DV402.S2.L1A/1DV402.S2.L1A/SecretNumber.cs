91bf88b0e28ae0123271eff57b71d976ff038167
unknown <ls222vy@NY108X10.lnu.se>
2014-10-01 12:41:22 +0200

    working with secretnumber

--- 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace _1DV402.S2.L1A
 {
    public class SecretNumber
-    {
+    { 
         private int _count;
         private int _number;
        
@@ -19,9 +19,18 @@ namespace _1DV402.S2.L1A
             _number = myRandom.Next(1, 101);
             _count = 0;
         }
+
         public bool MakeGuess(int number)
         {
             return true;
-        }    
-    }
+        }
+       
+        public SecretNumber()//konstructor
+        {
+            Initialize();//anropar initialize
+        }
+
+
+   }
+
 }
